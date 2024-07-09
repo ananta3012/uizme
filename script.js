0efcaf6b55245399be042440f47903a208ef515b
sukumar1210 <sukumar.rtg@gmail.com>
2024-07-09 06:23:43 +0530

    Added Waitlist confirmation

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const submitHandler = (e) => {
+
+const submitHandler = async (e) => {
+  document.getElementById('form-response').style.display = 'none';
   e.preventDefault();
-  const email = document.getElementById('email').value.toLowerCase();
+  const email = document.getElementById('email-input').value.toLowerCase();
   console.log(`https://script.google.com/macros/s/AKfycbxhcSw_QURK6SF7XZHIFSzqnYITZXyesL9bTyBtTVQcgyjLL7O_PehmaDhF6Rr9Ke9Q/exec?email=${email}`);
   console.log("Form submitted!")
-  fetch(`https://script.google.com/macros/s/AKfycbxhcSw_QURK6SF7XZHIFSzqnYITZXyesL9bTyBtTVQcgyjLL7O_PehmaDhF6Rr9Ke9Q/exec?email=${email}`,{
+  await fetch(`https://script.google.com/macros/s/AKfycbxhcSw_QURK6SF7XZHIFSzqnYITZXyesL9bTyBtTVQcgyjLL7O_PehmaDhF6Rr9Ke9Q/exec?email=${email}`,{
     method: 'GET',
   })
+  document.getElementById('email-input').value=''
+  document.getElementById('form-response').style.display = 'block';
+}
+
+const clearResponse = () => {
+  console.log("clearing");
+  document.getElementById('form-response').style.display = 'none';
 }
