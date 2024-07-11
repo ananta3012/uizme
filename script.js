b8990f1a03dc301503d5c447bfcf13104723a6dd
sukumar1210 <sukumar.rtg@gmail.com>
2024-07-11 16:21:56 +0530

    Added loading state while the form is being submitted

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 
 const submitHandler = async (e) => {
-  document.getElementById('form-response').style.display = 'none';
+  document.getElementById('submit').style.backgroundColor = '#afafaf';
+  document.getElementById('form-response').style.display = 'block';
+  document.getElementById('form-response').textContent = 'Please Wait...';
   e.preventDefault();
   const email = document.getElementById('email-input').value.toLowerCase();
   console.log(`https://script.google.com/macros/s/AKfycbxhcSw_QURK6SF7XZHIFSzqnYITZXyesL9bTyBtTVQcgyjLL7O_PehmaDhF6Rr9Ke9Q/exec?email=${email}`);
   console.log("Form submitted!")
+  // // miimc a delay of 2 sec
+  // await new Promise(r => setTimeout(r, 2000));
   await fetch(`https://script.google.com/macros/s/AKfycbxhcSw_QURK6SF7XZHIFSzqnYITZXyesL9bTyBtTVQcgyjLL7O_PehmaDhF6Rr9Ke9Q/exec?email=${email}`,{
     method: 'GET',
   })
   document.getElementById('email-input').value=''
-  document.getElementById('form-response').style.display = 'block';
+  document.getElementById('form-response').textContent = 'You are added to the waitlist✅';
+  document.getElementById('submit').style.backgroundColor = '#63432C';
 }
 
 const clearResponse = () => {
   console.log("clearing");
   document.getElementById('form-response').style.display = 'none';
+  document.getElementById('form-response').textContent = '';
+
 }
